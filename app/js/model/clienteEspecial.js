c570a38ae151c747b051051ddfde3db75b8d0f50
GeorgeLimaDev <george.lima@vtex.com.br>
2023-09-14 19:38:31 -0300

    basic implementation working

--- app/js/model/clienteEspecial.js
+++ app/js/model/clienteEspecial.js
@@ -2,4 +2,23 @@ class ClienteEspecial extends Cliente {
     constructor(nome, cpf, conta) {
         super(nome, cpf, conta);
     }
+    inserirDependente(dependente) {
+        this.dependentes.push(dependente);
+    }
+    removerDependente(conta) {
+        const dependenteARemover = this.pesquisarDependente(conta);
+        if (dependenteARemover) {
+            const indiceCliente = this.dependentes.indexOf(dependenteARemover);
+            if (indiceCliente > -1) {
+                console.log(`Dependente removido: ${dependenteARemover.nome}`);
+                this.dependentes.splice(indiceCliente, 1);
+            }
+        }
+    }
+    pesquisarDependente(cpf) {
+        return this.dependentes.find(cliente => cliente.cpf === cpf);
+    }
+    listarDependentes() {
+        return this.dependentes;
+    }
 }
--- app/ts/model/clienteEspecial.ts
+++ app/ts/model/clienteEspecial.ts
@@ -5,4 +5,27 @@ class ClienteEspecial extends Cliente {
     constructor(nome: string, cpf: string, conta: Conta) {
         super(nome, cpf, conta);
     }
+
+    inserirDependente (dependente: Cliente) {
+        this.dependentes.push(dependente);
+    }
+
+    removerDependente (conta: string) {
+        const dependenteARemover = this.pesquisarDependente(conta);
+        if (dependenteARemover) {
+            const indiceCliente = this.dependentes.indexOf(dependenteARemover);
+            if (indiceCliente > -1) {
+                console.log(`Dependente removido: ${dependenteARemover.nome}`);
+                this.dependentes.splice(indiceCliente, 1);
+            }
+        }
+    }
+
+    pesquisarDependente (cpf: string): Cliente {
+        return this.dependentes.find(cliente => cliente.cpf === cpf);
+    }
+
+    listarDependentes() {
+        return this.dependentes;
+    }
 }
